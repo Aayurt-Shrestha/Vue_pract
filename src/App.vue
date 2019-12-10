949591dc044a052e3394bb07f6ac9fa20cf721b5
Aayurt_Shrestha <aayurtshrestha@gmail.com>
2019-12-10 13:30:39 +0545

    third

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+    <h1>{{ headmsg }}</h1>
+    <my-footer></my-footer>
+    <my-content></my-content>
+    <my-content2></my-content2>
+  </div>
+</template>
+
+<script>
+import header from './myComponent/Header.vue'
+import footer from './myComponent/Footer.vue'
+import content from './myComponent/Content.vue'
+import content2 from './myComponent/Content2.vue'
+export default {
+  name: 'app',
+  components:{
+    "my-header" : header,
+    "my-footer" : footer,
+    "my-content" : content,
+    "my-content2" : content2
+  },
+  data () {
+    return {
+      headmsg: 'Welcome My Header'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+  text-align: center;
+  
+}
+
+</style>
---
+++ src/myComponent/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="footer">
+  	<p>I am Footer</p>
+	</div>
+</template>
+<script >
+	
+</script>
+<style scoped>
+	.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: grey;
+   color: white;
+   text-align: center;
+}
+</style>
